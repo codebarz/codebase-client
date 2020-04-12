c4b853c53bfa806bf73fd5118ee754d5d899bac2
whitehox <oketegah@gmail.com>
2020-04-12 02:19:30 +0100

    create editor component

---
+++ src/pages/editor/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/Header';
+import MainView from '../../components/MainView';
+
+function Editor() {
+  return (
+    <>
+      <Header />
+      <MainView />
+    </>
+  );
+}
+
+export default Editor;
